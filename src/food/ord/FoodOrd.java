2bddf7a821489ef375b041a8ee751f36f288726b
LENOVO <LENOVO@DESKTOP-2C4SQ4M>
2024-03-09 20:54:10 +0530

    first commit

--- src/food/ord/FoodOrd.java
+++ src/food/ord/FoodOrd.java
@@ -14,7 +14,8 @@ public class FoodOrd {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("hello");
     }
     
 }
